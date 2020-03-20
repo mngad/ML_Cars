c7bebaa5ec71e27837b8b37df7f9cacf119bf51c
Gavin Day <mengda@leeds.ac.uk>
2020-03-20 17:03:48 +0000

    removed the old build folder

--- Assets/Scripts/instantiate.cs
+++ Assets/Scripts/instantiate.cs
@@ -6,10 +6,11 @@ public class instantiate : MonoBehaviour
 {
 
     public GameObject Agent;
+    public int numCars = 0;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 1; i < 50; i++)
+        for (int i = 1; i < numCars; i++)
         {
             var newcar = Instantiate(Agent);
             newcar.name = "Car " + i;
